f2b4343e780f8ed908783fd73769e5f26160e416
Sale <sale@sales-imac.home>
2021-10-12 14:23:09 -0700

    Code Cube

---
+++ src/Block.java
@@ -0,0 +1,36 @@
+public class Block {
+
+
+    private int width;
+    private int length;
+    private int height;
+
+    public Block(int[] array) {
+        this.width = array[0];
+        this.length = array[1];
+        this.height = array[3];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    public int getVolume() {
+        return getWidth() * getLength() * getHeight();
+    }
+
+    public int getSurfaceAria() {
+        return getWidth() * getHeight() * 2 +
+                getWidth() * getLength() * 2
+                + getHeight() * getLength() * 2;
+    }
+
+
+}
